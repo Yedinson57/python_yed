50b63150a97847e28a2ec0070f7b72900f0a45ad
Yedinson57 <yedinson960@gmail.com>
2026-09-07 21:57:47 -0500

    ultimos_ejercicios

--- EjerciciosClase_Ins_Angie/Ejercicios_Diccionario/ejercicio4D.py
+++ EjerciciosClase_Ins_Angie/Ejercicios_Diccionario/ejercicio4D.py
@@ -1,2 +1,11 @@
-horario = {"Lunes":"Matematicas","Martes":"Ingles","Miercoles":"Programacion"}
+horario = {
+    "Lunes": "Matemáticas",
+    "Martes": "Inglés",
+    "Miércoles": "Programación"
+}
 
+dia = input("Ingrese un día: ")
+
+materia = horario.get(dia, "No hay una materia programada para ese día")
+
+print(materia)
